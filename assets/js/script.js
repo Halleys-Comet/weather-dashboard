1723cf9cf288bebd9d63faed1b4e62ffd3024f43
Halleys-Comet <jhalley230@gmail.com>
2021-01-22 22:20:08 -0600

    skeleton code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var searchTerm = document.querySelector('input-group');
+
+searchTerm = addEventListener('submit' inputHandler);
+
+function inputHandler(event) {
+
+}
+
+
+
+// function getCityWeather() {
+//     var apiURL = ('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=9b268063f024b15a46adf90cfb92fe67')
+//     fetch (apiURL)
+//     console.log(apiUrl)
+    
+// }
